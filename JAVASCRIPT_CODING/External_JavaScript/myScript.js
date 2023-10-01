fa66bdaa1a486b9ec1721c49ad97f50f0dc27ff6
Akanksha.dhomne <akanshadhomne06@gmail.com>
2023-10-01 11:23:05 +0530

    External javaScript commited

---
+++ JAVASCRIPT_CODING/External_JavaScript/myScript.js
@@ -0,0 +1,6 @@
+
+function myfunction(){
+    document.getElementById('demo').innerHTML = "paragraph changed.";
+    document.getElementById('demo').style.color="purple";
+    document.getElementById('demo').style.backgroundColor="pink";
+}
